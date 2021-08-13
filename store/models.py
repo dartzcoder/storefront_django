7333527a38964f411dddd4bfa517b50ff725ec20
Ubuntu <ubuntu@ubuntu.home>
2021-08-13 00:47:02 +0000

    test

--- store/models.py
+++ store/models.py
@@ -20,6 +20,8 @@ class Product(models.Model):
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
 
+    #this is a test
+
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
