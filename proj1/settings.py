7957384db5bc9c0f896d44cb2b0ca0aa0ca730c5
Ubuntu <ubuntu@ubuntu.home>
2021-08-13 00:41:18 +0000

    updated models for orm

--- proj1/settings.py
+++ proj1/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['192.168.1.160']
 
 INSTALLED_APPS = [
     'events.apps.EventsConfig',
+    'store',
+    'tags',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -84,8 +86,11 @@ WSGI_APPLICATION = 'proj1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'main',
+        'PASSWORD': '153'
     }
 }
 
--- store/models.py
+++ store/models.py
@@ -8,13 +8,15 @@ class Promotion(models.Model):
     
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
     
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_update - models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
 
@@ -46,13 +48,13 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES,
                                       default=PAYMENT_STATUS_PENDING)
-    customer = models.ForeignKey(Customer, on_delete.models.PROTECT)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.Protect)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_place=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     
 class Address(models.Model):
     street = models.CharField(max_length=255)
