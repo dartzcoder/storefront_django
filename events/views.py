d82465e2e4a8c4a596b68b94c3cb0f63345962be
Ubuntu <ubuntu@ubuntu.home>
2021-08-14 18:42:48 +0000

    template now displays query info from database

--- events/views.py
+++ events/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 # Create your views here.
-def calculate():
-    x = 1
-    y = 2
-    return x + y
+
 
 def say_hello(request):
-    return render(request, 'hello.html', {"name": [1,2,3]})
+    query_set = Product.objects.filter(store_price__range=(20, 30))   
+    return render(request, 'hello.html', {"name": "Dan", 'products': query_set})
