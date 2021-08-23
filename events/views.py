8f8ba35fa0594aee03ced419bd28bc8c9ab6f20b
Ubuntu <ubuntu@ubuntu.home>
2021-08-23 23:23:15 +0000

    updated tags app

--- events/views.py
+++ events/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.db.models import Q, F, Value, Func
+from django.db.models.functions import Concat
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product
+from store.models import Customer
+from store.models import Order
+from store.models import OrderItem
+from tags.models import TaggedItem
 # Create your views here.
 
 
+def generic_relationship_ex(request):
+    content_type = ContentType.objects.get_for_model(Product)
+    query_set = TaggedItem.objects\
+              .select_related('tag')\
+              .filter(
+                  content_type=content_type,
+                  object_id=1
+    )
+    return render(request, 'hello.html', {"name": "Dan's store", 'result': query_set})
+
 def say_hello(request):
-    query_set = Product.objects.filter(store_price__range=(20, 30))   
-    return render(request, 'hello.html', {"name": "Dan", 'products': query_set})
+    content_type = ContentType.objects.get_for_model(Product)
+    query_set = TaggedItem.objects\
+              .select_related('tag')\
+              .filter(
+                  content_type=content_type,
+                  object_id=1
+    )
+    return render(request, 'hello.html', {"name": "Dan's store", 'result': query_set})
