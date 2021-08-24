91741ca862e92d3f8c709ebfa185809bf1e528c9
Ubuntu <ubuntu@ubuntu.home>
2021-08-24 02:08:50 +0000

    added more functionality to admin site

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,27 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'store_price', 'inventory', 'inventory_status']
+    list_editable = ['store_price']
+    ordering = ['inventory']
+    list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
 admin.site.register(models.Order)
 admin.site.register(models.OrderItem)
